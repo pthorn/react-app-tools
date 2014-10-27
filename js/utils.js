da30c38d964db78415ae5fa84cf1fd1195c474aa
Thorn <p.thorn.ru@gmail.com>
2014-10-27 09:08:24 +0300

    Utils: add_subdomain()

--- js/utils.js
+++ js/utils.js
@@ -88,8 +88,25 @@ define([
     };
 
 
+    var add_subdomain = function(subdomain) {
+        var hostname_split = document.location.hostname.split(".");
+        if (hostname_split.length && hostname_split[0] == 'www') {    // TODO
+            hostname_split.splice(0, 1); // [xxx, site, com] -> [site, com]
+        }
+        hostname_split.unshift(subdomain);
+        var hostname = hostname_split.join('.');
+
+        if (document.location.port) {
+            hostname = hostname + ':' + document.location.port;
+        }
+
+        return hostname;
+    };
+
+
     return {
         NotificationMixin: NotificationMixin,
-        EventMixin:        EventMixin
+        EventMixin:        EventMixin,
+        add_subdomain:     add_subdomain
     };
 });
