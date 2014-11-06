3c6ca64f29ae731439a54e14534f3fcb7d4a5e3e
Thorn <p.thorn.ru@gmail.com>
2014-11-06 23:14:29 +0300

    form: merge ValidationMixin into FormMixin, add AdminFormMixin

--- js/form.jsx
+++ js/form.jsx
@@ -26,95 +26,8 @@ define([
     var FormMixin = {
 
         getInitialState: function () {
-            // TODO!!!
-            console.log('this.props.initialModel:', this.props.initialModel);
             return {
-                mode:     this.props.entityId ? 'edit' : 'new',
                 model:    this.props.initialModel || {},
-                entityId: null
-            };
-        },
-
-        componentWillMount: function () {
-            this.loadFormData();
-        },
-
-        //
-        // field api
-        //
-
-        // TODO checkboxes / radio
-        // checkbox: event.target.checked;
-        onFieldValueChanged: function (key, value) {
-            //console.log('Form.onFieldValueChanged(): ', key, value);
-
-            // TODO setState()!
-            // TODO
-            //this.setState({
-                this.state.model[key] = value;
-                this.state.dirty[key] = true;
-            //});
-
-            this.setState({
-                model: this.state.model,
-                dirty: this.state.dirty
-            });
-
-            /*
-            this.setState(React.addons.update(this.state, {
-                model: {$merge: {}},
-                dirty: {}
-            }));
-            */
-
-            this.validate(key);
-        },
-
-        //
-        // form api
-        //
-
-        submitForm: function (e) {
-            var c = this,
-                p = this.props,
-                s = this.state;
-
-            e.preventDefault();
-
-            c.setState({submitAttempted: true});
-
-            if (!this.validate()) {
-                console.log('invalid', s.errors);
-                c.showNotification('Ошибки в форме', undefined, 'warning');  // TODO list errors!
-                return;
-            }
-
-            c.saveFormData();
-        },
-
-        isDirty: function () {
-            return !_.isEmpty(this.state.dirty);
-        }
-    };
-
-
-    /**
-     * Validation mixin, to be used together with the FormMixin
-     *
-     * validators: {
-     *     '*':     [<validator>, ...],
-     *     '<key>': [<validator>, ...],
-     *     ...
-     * }
-     *
-     * Validator interface: function(model, key, val, fail) {
-     *     if (<invalid>) fail(key, 'message');
-     * }
-     *
-     */
-    var ValidationMixin = {
-        getInitialState: function () {
-            return {
                 errors: {}, // {key: 'string', ...}
                 dirty: {},  // {key: true}
                 submitAttempted: false
@@ -134,6 +47,8 @@ define([
                 }
             }, this);
         },
+
+        // validation
 
         validate: function (key) {
             var model = this.state.model,
@@ -185,7 +100,7 @@ define([
             this.validators[key] = this.validators[key].concat(validators);
         },
 
-        addServerErrors: function (server_errors) {
+        addServerValidationErrors: function (server_errors) {
             // TODO
         },
 
@@ -202,6 +117,12 @@ define([
             });
         },
 
+        isDirty: function () {
+            return !_.isEmpty(this.state.dirty);
+        },
+
+        // field api
+
         fieldShouldShowError: function (key) {
             var is_valid = !_.has(this.state.errors, key),
                 is_dirty = _.has(this.state.dirty, key);
@@ -211,6 +132,138 @@ define([
 
         fieldMessage: function (key) {
             return this.state.errors[key];
+        },
+
+        onFieldValueChanged: function (key, value) {
+            //console.log('Form.onFieldValueChanged(): ', key, value);
+
+            this.state.model[key] = value;
+            this.state.dirty[key] = true;
+
+            this.setState({
+                model: this.state.model,
+                dirty: this.state.dirty
+            });
+
+            /*
+            this.setState(React.addons.update(this.state, {
+                model: {$merge: {}},
+                dirty: {}
+            }));
+            */
+
+            this.validate(key);  // TODO
+        },
+
+        // form submission
+
+        submitForm: function (e) {
+            var c = this,
+                p = this.props,
+                s = this.state;
+
+            e.preventDefault();
+
+            c.setState({submitAttempted: true});
+
+            if (!this.validate()) {
+                console.log('invalid', s.errors);
+                c.showNotification('Ошибки в форме', undefined, 'warning');  // TODO list errors!
+                return;
+            }
+
+           // TODO catch any exceptions!
+            c.saveModel();
+
+            // TODO after submit
+        },
+
+        // TODO wrap everything in this?
+        handleErrors: function (promise) {
+            return promise.catch(function () {
+
+            });
+        }
+    };
+
+
+    /**
+     * methods to be implemented by the user
+     *   loadDefaults: function ()
+     *   loadEntity: function (entity_id) { return fooStore.loadFoo(entity_id); }
+     *   createEntity: function (model) { return fooStore.createFoo(model); }
+     *   updateEntity: function (entity_id, model) { return fooStore.updateFoo(entity_id, model); }
+     *   afterSaved: function (data)
+     */
+    var AdminFormMixin = {
+        getInitialState: function () {
+            return {
+                mode:     null,
+                entityId: null
+            };
+        },
+
+        loadModel: function (props) {
+            var c = this,
+                entity_id = props.params.id;
+
+            console.log("AdminFormMixin loadModel", props.name, props.params);
+
+            if (_.isUndefined(entity_id)) {
+                c.setState({
+                    mode: 'new',
+                    model: c.loadDefaults()  // TODO allow it to return promise?
+                });
+            } else {
+                c.loadEntity(entity_id).then(function (data) {
+                    c.setState({
+                        mode: 'edit',
+                        entityId: entity_id,
+                        model: data.data
+                    });
+                }).catch(function (error) {
+                    console.log('loadEntity error:', error);
+                    if (error.reason == 'rest-error') {
+                        c.showNotification('Ошибка', JSON.stringify(error.json), 'error');  // TODO
+                    }
+                });
+            }
+        },
+
+        saveModel: function () {
+            var c = this,
+                p = this.props,
+                s = this.state;
+
+            var promise;
+            if (s.mode === 'new') {
+                promise = c.createEntity(s.model);
+            } else if (s.mode === 'edit') {
+                promise = c.updateEntity(s.entityId, s.model);
+            } else {
+                console.warn('AdminFormMixin.saveModel(): bad s.mode: ', s.mode);
+                return;  // ???
+            }
+
+            promise.then(function (data) {
+                c.showNotification('Объект сохранен', '', 'success');
+                c.onSaved && c.onSaved(data);   // TODO
+            }).catch(function (error) {
+                if (error.reason == 'invalid') {
+                    c.showNotification('Ошибки валидации', JSON.stringify(error.json), 'warning');  // TODO
+                    c.addServerValidationErrors(error.json.errors);
+                } else if (error.reason == 'rest-error') {
+                    c.showNotification('Ошибка', JSON.stringify(error.json), 'error');  // TODO
+                }
+            });
+        },
+
+        componentWillMount: function () {
+            this.loadModel(this.props);
+        },
+
+        componentWillReceiveProps: function (new_props) {
+            this.loadModel(new_props);
         }
     };
 
@@ -471,7 +524,6 @@ define([
             var form = this.props.form,
                 name = this.props.name;
 
-            console.log('Textarea.onChange:', name, e.target.value);
             // TODO deserialize value
             form.onFieldValueChanged(name, e.target.value);
         }
@@ -519,7 +571,7 @@ define([
 
     return {
         FormMixin:         FormMixin,
-        ValidationMixin:   ValidationMixin,
+        AdminFormMixin:    AdminFormMixin,
         FormGroup:         FormGroup,
         TextInput:         TextInput,
         Checkbox:          Checkbox,
