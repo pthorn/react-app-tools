d9f6a1d2f39dcb797a73046dfa9c4ac978a1705f
Thorn <p.thorn.ru@gmail.com>
2016-02-25 16:42:46 +0300

    Rest: update module to ES6 format

--- js/rest.js
+++ js/rest.js
@@ -1,241 +1,234 @@
 'use strict';
 
-define([
-    'lodash',
-    'eventemitter'
-], function (
-    _,
-    EventEmitter
-) {
+import _ from 'lodash';
+import EventEmitter from 'eventemitter';
+
+
+/**
+ * config:
+ *   url_prefix:  string, default '/rest/', could be '//rest.me.com/api/'
+ *   csrf_token:  string or callable, default null
+ *
+ * events:
+ *   'rest-error'
+ *   'http-error-401'
+ *   'http-error-403'
+ *   'http-error'      http status is 4xx or 5xx but not 401 or 403
+ *   'start-request'
+ *   'end-request'
+ */
+export const Rest = function (config_) {
+    var self = this;
+
+    var config = $.extend({
+        url_prefix:       '/rest/',
+        csrf_token: null
+    }, config_ || {});
+
+    if (config.url_prefix.indexOf('/', this.length - 1) === -1) {
+        config.url_prefix = config.url_prefix + '/';
+    }
+
+    var requests_in_progress = 0;
+
     /**
-     * config:
-     *   url_prefix:  string, default '/rest/', could be '//rest.me.com/api/'
-     *   csrf_token:  string or callable, default null
+     * opts:
+     *   url
+     *   method
+     *   data: query string if GET else JSON request body
+     *   dataType: default is 'json'
+     *   contentType: default is 'application/json'
      *
-     * events:
-     *   'rest-error'
-     *   'http-error-401'
-     *   'http-error-403'
-     *   'http-error'      http status is 4xx or 5xx but not 401 or 403
-     *   'start-request'
-     *   'end-request'
+     *   @return promise:
+     *     .then(arg): json response
+     *     .catch(arg): {reason: 'string', json|xhr: ...}
      */
-    var Rest = function (config_) {
-        var self = this;
+    this.request = function (opts) {
 
-        var config = $.extend({
-            url_prefix:       '/rest/',
-            csrf_token: null
-        }, config_ || {});
+        var defaultOpts = {
+            dataType: 'json',
+            contentType: 'application/json'
+        };
 
-        if (config.url_prefix.indexOf('/', this.length - 1) === -1) {
-            config.url_prefix = config.url_prefix + '/';
+        if (opts.method != 'GET' && config.csrf_token !== null) {
+            var csrf_token = config.csrf_token;
+            if (_.isFunction(csrf_token)) {
+                csrf_token = csrf_token();
+            }
+
+            defaultOpts.headers = {
+                'X-CSRF-Token': csrf_token
+            };
         }
 
-        var requests_in_progress = 0;
+        if (opts.data && opts.method != 'GET') {
+            opts.data = JSON.stringify(opts.data);
+        }
 
-        /**
-         * opts:
-         *   url
-         *   method
-         *   data: query string if GET else JSON request body
-         *   dataType: default is 'json'
-         *   contentType: default is 'application/json'
-         *
-         *   @return promise:
-         *     .then(arg): json response
-         *     .catch(arg): {reason: 'string', json|xhr: ...}
-         */
-        this.request = function (opts) {
+        // TODO _nocache=`new Date().getTime()`
+        // TODO https://docs.angularjs.org/api/ng/service/$http#security-considerations
 
-            var defaultOpts = {
-                dataType: 'json',
-                contentType: 'application/json'
-            };
-
-            if (opts.method != 'GET' && config.csrf_token !== null) {
-                var csrf_token = config.csrf_token;
-                if (_.isFunction(csrf_token)) {
-                    csrf_token = csrf_token();
-                }
-
-                defaultOpts.headers = {
-                    'X-CSRF-Token': csrf_token
-                };
+        return new Promise(function (resolve, reject) {
+            if (requests_in_progress++ == 0) {
+                self.emit('start-request');
             }
 
-            if (opts.data && opts.method != 'GET') {
-                opts.data = JSON.stringify(opts.data);
-            }
-
-
-            return new Promise(function (resolve, reject) {
-                if (requests_in_progress++ == 0) {
-                    self.emit('start-request');
-                }
-
-                $.ajax($.extend({}, defaultOpts, opts, {
-                    success: function (json) {
-                        if (--requests_in_progress == 0) {
-                            self.emit('end-request');
-                        }
-
-                        // TODO what if response is not JSON?
-
-                        if (json.status === 'ok') {
-                            resolve(json);
-                        }
-
-                        if (json.status !== 'ok') {
-                            self.emit('rest-error', json);
-                            reject({reason: 'rest-error', json: json});
-                        }
-                    },
-
-                    error: function (jqXhr, textStatus, errorThrown) {
-                        if (--requests_in_progress == 0) {
-                            self.emit('end-request');
-                        }
-
-                        if (jqXhr.status == 401) {
-                            self.emit('http-error-401', jqXhr);
-                        } else if  (jqXhr.status == 403) {
-                            self.emit('http-error-403', jqXhr);
-                        } else {
-                            self.emit('http-error', jqXhr);
-                        }
-
-                        reject({reason: 'http-error', xhr: jqXhr});
+            $.ajax($.extend({}, defaultOpts, opts, {
+                success: function (json) {
+                    if (--requests_in_progress == 0) {
+                        self.emit('end-request');
                     }
-                }));
-            });
-        };
 
-        // CRUD methods
+                    // TODO what if response is not JSON?
 
-        this.getList = function (entity, params) {
-            var url = config.url_prefix + entity;
+                    if (json.status === 'ok') {
+                        resolve(json);
+                    }
 
-            var qs = {};
+                    if (json.status !== 'ok') {
+                        self.emit('rest-error', json);
+                        reject({reason: 'rest-error', json: json});
+                    }
+                },
 
+                error: function (jqXhr, textStatus, errorThrown) {
+                    if (--requests_in_progress == 0) {
+                        self.emit('end-request');
+                    }
 
-            if (_.isObject(params)) {
-                if (params.start) {
-                    qs.s = params.start;
+                    if (jqXhr.status == 401) {
+                        self.emit('http-error-401', jqXhr);
+                    } else if  (jqXhr.status == 403) {
+                        self.emit('http-error-403', jqXhr);
+                    } else {
+                        self.emit('http-error', jqXhr);
+                    }
+
+                    reject({reason: 'http-error', xhr: jqXhr});
                 }
+            }));
+        });
+    };
 
-                if (params.limit) {
-                    qs.l = params.limit;
-                }
+    // CRUD methods
 
-                if (_.isString(params.order)) {
-                    qs.o = params.order;
-                } else if (_.isObject(params.order) && params.order.col) {
-                    qs.o = (params.order.dir == 'desc' ? '-' : '') + params.order.col;
-                }
+    this.getList = function (entity, params) {
+        var url = config.url_prefix + entity;
 
-                if (params.search) {
-                    qs.q = params.search;
-                }
+        var qs = {};
 
-                if (params.filters) {
-                    $.each(params.filters, function (key, val) {
-                        if (!val && val !== 0) {
-                            return;
-                        }
 
-                        var value = val,
-                            op = 'e';
-                        if (val instanceof Array) {
-                            op = val[0];
-                            value = val[1];
-                        }
-
-                        qs['f' + op + '_' + key] = value;
-                    });
-                }
+        if (_.isObject(params)) {
+            if (params.start) {
+                qs.s = params.start;
             }
 
-            return this.request({
-                url: url,
-                method: 'GET',
-                data: qs
-            });
-        };
+            if (params.limit) {
+                qs.l = params.limit;
+            }
 
-        this.getEntityById = function (entity, id) {
-            var url = config.url_prefix + entity + '/' + id;
-            return this.request({
-                url: url,
-                method: 'GET'
-            });
-        };
+            if (_.isString(params.order)) {
+                qs.o = params.order;
+            } else if (_.isObject(params.order) && params.order.col) {
+                qs.o = (params.order.dir == 'desc' ? '-' : '') + params.order.col;
+            }
 
-        this.createEntity = function (entity, data) {
-            var url = config.url_prefix + entity;
-            return this.request({
-                url: url,
-                method: 'POST',
-                data: data
-            });
-        };
+            if (params.search) {
+                qs.q = params.search;
+            }
 
-        this.updateEntityForId = function (entity, id, data) {
-            var url = config.url_prefix + entity + '/' + id;
-            return this.request({
-                url: url,
-                method: 'PUT',
-                data: data
-            });
-        };
+            if (params.filters) {
+                $.each(params.filters, function (key, val) {
+                    if (!val && val !== 0) {
+                        return;
+                    }
 
-        this.deleteById = function (entity, id) {
-            var url = config.url_prefix + entity + '/' + id;
-            return this.request({
-                url: url,
-                method: 'DElETE'
-            });
-        };
+                    var value = val,
+                        op = 'e';
+                    if (val instanceof Array) {
+                        op = val[0];
+                        value = val[1];
+                    }
 
-        // low level methods
+                    qs['f' + op + '_' + key] = value;
+                });
+            }
+        }
 
-        this.get = function (url, data) {
-            return this.request({
-                url: url,
-                method: 'GET',
-                data: data
-            });
-        };
-
-        this.post = function (url, data) {
-            return this.request({
-                url: url,
-                method: 'POST',
-                data: data
-            });
-        };
-
-        this.put = function (url, data) {
-            return this.request({
-                url: url,
-                method: 'PUT',
-                data: data
-            });
-        };
-
-        this.del = function (url, data) {
-            return this.request({
-                url: url,
-                method: 'DELETE',
-                data: data
-            });
-        };
+        return this.request({
+            url: url,
+            method: 'GET',
+            data: qs
+        });
     };
 
-    _.extend(Rest.prototype, EventEmitter.prototype);
-
-
-    return {
-        Rest: Rest
+    this.getEntityById = function (entity, id) {
+        var url = config.url_prefix + entity + '/' + id;
+        return this.request({
+            url: url,
+            method: 'GET'
+        });
     };
-});
+
+    this.createEntity = function (entity, data) {
+        var url = config.url_prefix + entity;
+        return this.request({
+            url: url,
+            method: 'POST',
+            data: data
+        });
+    };
+
+    this.updateEntityForId = function (entity, id, data) {
+        var url = config.url_prefix + entity + '/' + id;
+        return this.request({
+            url: url,
+            method: 'PUT',
+            data: data
+        });
+    };
+
+    this.deleteById = function (entity, id) {
+        var url = config.url_prefix + entity + '/' + id;
+        return this.request({
+            url: url,
+            method: 'DElETE'
+        });
+    };
+
+    // low level methods
+
+    this.get = function (url, data) {
+        return this.request({
+            url: url,
+            method: 'GET',
+            data: data
+        });
+    };
+
+    this.post = function (url, data) {
+        return this.request({
+            url: url,
+            method: 'POST',
+            data: data
+        });
+    };
+
+    this.put = function (url, data) {
+        return this.request({
+            url: url,
+            method: 'PUT',
+            data: data
+        });
+    };
+
+    this.del = function (url, data) {
+        return this.request({
+            url: url,
+            method: 'DELETE',
+            data: data
+        });
+    };
+};
+
+_.extend(Rest.prototype, EventEmitter.prototype);
